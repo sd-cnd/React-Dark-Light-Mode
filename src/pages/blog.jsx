da21e240c6b7bd85945ffa8d2e7f9da82f0438ea
sd-cnd <sumitdey.cnd@gmail.com>
2024-07-08 13:31:12 +0530

    First commit

---
+++ src/pages/blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div className='page'>
+      <h1>This is the blog page</h1>
+      <p>Read out latest blogs here!</p>
+    </div>
+  )
+}
+
+export default Blog
